cd38dad49f4d03deec8aae63ee6139c03ae29727
tevzek <tevzek.matevzek@gmail.com>
2018-08-03 08:55:22 +0200

    helo darko my old friend

---
+++ DrVigorIgor/Assets/Scripts/JumpToAnotherScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class JumpToAnotherScene : MonoBehaviour
+{
+
+    
+
+    public void jumpToNextScene(string scena)
+    {
+        SceneManager.LoadScene(scena);
+
+    }
+}
---
+++ DrVigorIgor/Assets/Scripts/Medication.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+    public class Medication
+    {
+        public int timesPerDay { get; set; }
+    public string name { get; set; }
+    public List<float> drugTime { get; set; }
+
+        public Medication(string name, int timesPerDay)
+        {
+            timesPerDay = this.timesPerDay;
+            name = this.name;
+            for (int i = 0; i < timesPerDay; i++)
+            {
+                drugTime.Add(24/timesPerDay*i);
+            }
+        }
+    }
